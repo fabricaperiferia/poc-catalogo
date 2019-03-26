d96e04922da6e8e2ba43768f919b362e03937181
raphael-martinez-periferia <raphael.martinez@periferia-it.com>
2019-03-25 21:42:31 -0500

    get filter catalogue

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+'use strict'
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let mongoose = require('mongoose');
+let bodyParser = require('body-parser');
+let indexRouter = require('./routes/index');
+let catalogueRouter = require('./routes/catalogue');
 
-var app = express();
+let app = express();
 
-// view engine setup
+//Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//conectDB
+mongoose.connect('mongodb://localhost/poc-catalogo');
+
+var db = mongoose.connection;
+
+// Vista de cambios
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -19,15 +31,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+//Routes
+app.use('/', indexRouter);
+app.use('/catalogue', catalogueRouter);
+
+// Error 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
---
+++ routes/catalogue.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/*Lista catalogo */
+router.get('/', function(req, res, next) {
+  res.status(200).send({
+    message:"ok",
+    product:[]
+  });
+});
+
+/*Lista catalogo con filtro */
+router.get('/:filter', function(req, res, next) {
+  console.log(req.params.filter)
+  res.send({
+    message:req.params.filter
+  });
+});
+module.exports = router;
