0bf96c2f861700283f5cc476603071ee210a3e7a
raphael-martinez-periferia <raphael.martinez@periferia-it.com>
2019-03-26 00:40:45 -0500

    modelo de catalogo

--- models/catalogue.js
+++ models/catalogue.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const catalogo= Schema({
-    nombre:{type:String}
-})
+    nombre:String,
+    precio:String,
+    presentacion:String,
+    categoria:String,
+   })
 module.exports = mongoose.model('catalogo', catalogo)
--- routes/catalogue.js
+++ routes/catalogue.js
@@ -4,20 +4,22 @@ const catalogo = require('../models/catalogue')
 
 /*Lista catalogo */
 router.get('/', function(req, res, next) {
-  catalogo.find({},(catalogue) =>{
+  // catalogo.createCollection();
+  catalogo.find({},(err,catalogue) =>{
     res.status(200).send({
       message:"ok",
       product:catalogue
     });
   })
- 
-});
+ });
 
 /*Lista catalogo con filtro */
 router.get('/:filter', function(req, res, next) {
-  console.log(req.params.filter)
-  res.send({
-    message:req.params.filter
-  });
+  catalogo.find({nombre:req.params.filter},(err,catalogue) =>{
+    res.status(200).send({
+      message:"ok",
+      product:catalogue
+    });
+  })
 });
 module.exports = router;
