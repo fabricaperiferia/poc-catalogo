e7650969e77adcd347974341e081a6d5e35ae6a9
raphael-martinez-periferia <raphael.martinez@periferia-it.com>
2019-04-11 11:47:21 -0500

    Raphael Martinez jwt poc-catalogo

--- controllers/Catalogo.js
+++ controllers/Catalogo.js
@@ -3,38 +3,42 @@
 var catalogo = require('../models/catalogue');
 var jose = require('node-jose');
 const mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
 
 
 /** 
  @description Retorna el catálogo completo de la aplicación 
 **/
-module.exports.catalogueGET = function catalogueGET (req, res, next) {
-  catalogo.find({},(err,catalogue) =>{
-   res.status(200).send({
-      message:"ok",
-      product:catalogue
-    });
-  })
-};
+module.exports.catalogueGET = function catalogueGET(req, res, next) {
+  catalogo.find({}, (err, catalogue) => {
+    res.status(200).send({
+          message: "ok",
+          product: jwt.sign(JSON.stringify(catalogue), 'test')
+        });
+    })
+  }
 
 
 /**
- @description Lista catálogo de la aplicación según el respectivo filtro
+ * @author Raphael Martinez
+ * @description Lista catálogo de la aplicación según el respectivo filtro
  **/
+
 module.exports.getCatalogueByFilter = function getCatalogueByFilter(req, res, next) {
   let valueFilter = req.swagger.params['filter'].value;
-   catalogo.find({$or:[{nombre:{$regex:valueFilter,$options: 'i'}},{presentacion:{$regex:valueFilter,$options: 'i'}},
+   catalogo.find({$or:[{nombre:{$regex:valueFilter,$options: 'i'}},
+   {presentacion:{$regex:valueFilter,$options: 'i'}},
   {categoria:{$regex:valueFilter,$options: 'i'}}]}, (err,catalogue) =>{
-    if (catalogue.length === 0 ){
+    if (catalogue.length !== 0 ){
     res.status(200).send({
       message:"ok",
-      product:catalogue
+      product:jwt.sign(JSON.stringify(catalogue), 'test')
     });
   }
   else {
     res.status(201).send({
       message:"No se encuentra parametros para este filtro",
-      product:catalogue
+      product:jwt.sign(JSON.stringify(catalogue), 'test')
     });
   }
   })
